1ecc89d1a4deb03e80ba8bf377a113a66d604ee2
Florian Kirmaier <florian.kirmaier@gmail.com>
2019-04-04 11:10:01 +0200

    added the option to disable rendering for everything except snapshots

--- modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/QuantumToolkit.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/QuantumToolkit.java
@@ -182,6 +182,12 @@ public final class QuantumToolkit extends Toolkit {
                 return result;
             });
 
+    private static boolean renderOnlySnapshots =
+            AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> {
+                Boolean result = Boolean.getBoolean("quantum.renderonlysnapshots");
+                return result;
+            });
+
     private static boolean noRenderJobs =
             AccessController.doPrivileged((PrivilegedAction<Boolean>) () -> {
                 Boolean result = Boolean.getBoolean("quantum.norenderjobs");
@@ -430,8 +436,12 @@ public final class QuantumToolkit extends Toolkit {
     }
 
     @Override public Future addRenderJob(RenderJob r) {
+        return addRenderJob(r,false);
+    }
+
+    public Future addRenderJob(RenderJob r, boolean isSnapshot){
         // Do not run any render jobs (this is for benchmarking only)
-        if (noRenderJobs) {
+        if (noRenderJobs || (!isSnapshot && renderOnlySnapshots)) {
             CompletionListener listener = r.getCompletionListener();
             if (r instanceof PaintRenderJob) {
                 ((PaintRenderJob)r).getScene().setPainting(false);
@@ -1478,7 +1488,7 @@ public final class QuantumToolkit extends Toolkit {
 
         final CountDownLatch latch = new CountDownLatch(1);
         re.setCompletionListener(job -> latch.countDown());
-        addRenderJob(re);
+        addRenderJob(re, true);
 
         do {
             try {
